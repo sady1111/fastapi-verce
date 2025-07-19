483756ff074ee6e5f2b7e5416cb84f34f476e693
sady1111 <sady.1@live.com>
2025-07-19 09:57:15 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,20 +1,8 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from twilio.twiml.voice_response import VoiceResponse
-import os
-
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-# Allow all origins (for testing only — restrict this in production)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.post("/call", response_class=PlainTextResponse)
 async def call():
